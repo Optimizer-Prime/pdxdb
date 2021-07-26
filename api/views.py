2619fe724286d2fdc68dea4e293f782d69029626
Optimizer-Prime <stu3cla@protonmail.com>
2021-07-26 02:41:16 -0400

    viewset and router permissions working

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from pdx.models import Pdx
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PdxSerializer
 
 
 class PdxViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Pdx.objects.all()
     serializer_class = PdxSerializer
